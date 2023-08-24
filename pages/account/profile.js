8f3d634d2698d986bc25d8ba96149437202d4658
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-08-24 14:55:09 +0100

    no commit messagee

--- pages/account/profile.js
+++ pages/account/profile.js
@@ -13,11 +13,12 @@ export default function () {
     const {data:session} = useSession();
     const router = useRouter();
     const [userPosts,setUserPosts] = React.useState([]);
+    const userEmail = session?.user?.email;
 
     const handleGetUserPost = async () => {
         const q = query(
             collection(db,'posts'),
-            where('author','==',session.user.email),
+            where('author','==',userEmail),
             orderBy('postedAt','desc')
         );
         const onSnapShot = await getDocs(q);
