edfec0d590b4ab0c669ad2a7c913e7a1a0390133
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-09-01 16:41:57 +0100

    Revert "configure firestore adapter"
    This reverts commit 9e04fe61fa66768010f93df63e8b932555cd0b88.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
-import { cert } from "firebase-admin/app";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
 
 export const authOptions = {
     providers:[
@@ -15,13 +13,6 @@ export const authOptions = {
             clientSecret:process.env.GITHUB_CLIENT_SECRET,
         })
     ],
-    adapter: FirestoreAdapter({
-        credential: cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,"\n") : undefined,
-        })
-    }),
     callbacks:{
         async session({session,user}) {
             // console.log(user);
