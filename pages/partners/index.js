21f489efdf7e6d64199a9bf9ef75de5457eea8dc
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-09-07 11:47:24 +0100

    add partners page

---
+++ pages/partners/index.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+
+export default function Paryners() {
+    return (
+        <>
+        <Head>
+            <link rel="icon" href="/facepal_icon_logo.ico" />
+            <meta name="description" content="The Coolest way to connect with friends and hold money" />
+            <meta name="keywords" content="facepal" />
+            <title>facepal | Partners</title>
+        </Head>
+        <main className="px-4 py-6 sm:px-16 sm:py-16 md:px-24 md:py-20">
+            <h1 className="text-4xl text-gray-700">
+                Choose from our list of partners to access financial services
+            </h1>
+            <section className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 mt-8">
+                {
+                    '111111111'.split('').map(partner => (
+                        <article className="border border-gray-200 rounded-lg shadow-md p-4">
+                            <div className="flex flex-row justify-between items-center gap-4 mb-4">
+                                <blockquote>
+                                    <span className="text-2xl">Company name</span>
+                                    <span>+2349065487956</span>
+                                </blockquote>
+                                <Image 
+                                width={60}
+                                height={60}
+                                // className="h-24"
+                                src='https://images.pexels.com/photos/2506776/pexels-photo-2506776.jpeg'
+                                alt="Company logo"/>
+                            </div>
+
+                            <Link href='#' className="flex flex-row  justify-center gap-2 bg-violet-950 rounded-lg p-4 text-white">
+                                <span>View Profile</span>
+                            </Link>
+                        </article>
+                    ))
+                }
+            </section>
+        </main>
+        </>
+    )
+}
