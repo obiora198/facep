3027960e4839c4a91f9177aef3602c34f96a0dc1
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-09-17 15:18:12 +0100

    fix router error in vercel build

--- pages/feeds.js
+++ pages/feeds.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Head from 'next/head';
 import Link from 'next/link';
